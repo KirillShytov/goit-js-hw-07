bfa6f726b169f3e642c5ae35d3fb418daf74f26b
KirillShytov <sitovk587@gmail.com>
2023-12-17 02:21:21 +0200

    ccc

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,6 @@
-
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach((value) => {
+  console.log(`Category: ${value.firstElementChild.textContent}`);
+  console.log(`Elements: ${value.lastElementChild.children.length}`);
+});
