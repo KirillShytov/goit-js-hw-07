cc75f3b86db4d8f6550acf1f6f1426f186159ded
KirillShytov <sitovk587@gmail.com>
2023-12-19 15:29:47 +0200

    fin

--- js/task-03.js
+++ js/task-03.js
@@ -3,5 +3,5 @@ const output = document.querySelector("#name-output");
 
 formInput.addEventListener("input", (event) => {
   const trimValue = event.target.value.trim();
-  output.textContent = trimValue || "Anonymous";
+  output.textContent = trimValue !== "" ? trimValue : "Anonymous";
 });
--- js/task-05.js
+++ js/task-05.js
@@ -1,21 +1,5 @@
-// const bodyElement = document.querySelector("body");
-// const changeColorButton = document.querySelector("button.change-color");
-// const colorSpan = document.querySelector("span.color");
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// changeColorButton.addEventListener("click", () => {
-//   const color = getRandomHexColor;
-//   bodyElement.style.backgroundColor = color;
-//   colorSpan.textContent = color;
-// });
-
 const bodyElement = document.querySelector("body");
-const changeColorButton = document.querySelector("button.change-color");
+const changeColorButton = document.querySelector(".change-color");
 const colorSpan = document.querySelector("span.color");
 
 function getRandomHexColor() {
